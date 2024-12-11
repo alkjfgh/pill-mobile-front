4f9f46ad99ca6b1694912743d3c36d875f5d8162
Spica-blue <heo1356@gmail.com>
2024-12-11 14:20:42 +0900

    설정 작업중

---
+++ screens/SettingScreen.js
@@ -0,0 +1,12 @@
+import { View, Text, Button, SafeAreaView } from "react-native";
+import styles from "../style/MainStyle";
+
+const SettingScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Setting</Text>
+    </View>
+  );
+};
+
+export default SettingScreen;
